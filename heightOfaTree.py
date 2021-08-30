652d025e039838cef721783c7d8b89136a3923dd
Vikash Jha <jha2020vikash@yahoo.com>
2021-08-30 13:25:00 +0545

    height of tree found

---
+++ heightOfaTree.py
@@ -0,0 +1,105 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+def height(root) :
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return 1
+    leftHeight=height(root.left)
+    rightHeight=height(root.right)
+    largest_height=max(leftHeight,rightHeight)
+    return 1+largest_height
+    
+    
+	#Your code goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+# Main
+root = takeInput()
+
+h = height(root)
+print(h)
